54fa8061b5e10caddd8ef194c5d70728cce49d32
Harshit <harshittrip2002@gmail.com>
2021-09-26 13:59:28 +0530

    change

--- register.js
+++ register.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
-// mongoose.connect("mongodb://localhost:27017/about_student",{useCreateIndex:true,useFindAndModify:false,useNewUrlParser:true,useUnifiedTopology:true})
-// .then(()=>{
-//     console.log("connection is succesful");
-// }).catch((err)=>{
-//     console.log("Not connected");
-// })
-
-mongoose.connect("mongodb+srv://Harshittripathi:Harshit@2841@main.hwlhz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{useCreateIndex:true,useFindAndModify:false,useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect("mongodb://localhost:27017/about_student",{useCreateIndex:true,useFindAndModify:false,useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=>{
     console.log("connection is succesful");
 }).catch((err)=>{
     console.log("Not connected");
 })
+
+// mongoose.connect("mongodb+srv://Harshittripathi:Harshit@2841@main.hwlhz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{useCreateIndex:true,useFindAndModify:false,useNewUrlParser:true,useUnifiedTopology:true})
+// .then(()=>{
+//     console.log("connection is succesful");
+// }).catch((err)=>{
+//     console.log("Not connected");
+// })
 
 
 const studentSchema = new mongoose.Schema({
